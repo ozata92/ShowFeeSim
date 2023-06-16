da7f75d40de48c60449120833e5d0a2d06569cd4
Taisuke Ozaki <97082787+ozata92@users.noreply.github.com>
2023-06-17 00:43:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,42 @@
 console.log("Script.js loaded");
 
-// HTMLの各要素を取得
-let widthSetting = document.getElementById('widthSetting');
-console.log('widthSetting element:', widthSetting);
-let typeSetting = document.getElementById('typeSetting');
-console.log('typeSetting element:', typeSetting);
-let ticketPrice = document.getElementById('ticketPrice');
-console.log('ticketPrice element:', ticketPrice);
-let numberOfSeats = document.getElementById('numberOfSeats');
-console.log('numberOfSeats element:', numberOfSeats);
-let numberOfShows = document.getElementById('numberOfShows');
-console.log('numberOfShows element:', numberOfShows);
-let subsidy = document.getElementById('subsidy');
-console.log('subsidy element:', subsidy);
-let result = document.getElementById('result');
-console.log('result element:', result);
-let calculationProcess = document.getElementById('calculationProcess');
-console.log('calculationProcess element:', calculationProcess);
-
-// 各ステップへのボタンを取得
-let toStep2 = document.getElementById('toStep2');
-let toStep3 = document.getElementById('toStep3');
-let toStep4 = document.getElementById('toStep4');
-let toStep5 = document.getElementById('toStep5');
-let toStep6 = document.getElementById('toStep6');
-
-// 全てのステップを取得
-let steps = document.querySelectorAll('.step');
-
-// ステップを切り替える関数
-function switchStep(nextIndex) {
-    console.log('Switching to step:', nextIndex + 1);
+// ステップの変更関数
+function switchStep(step) {
+    let steps = document.querySelectorAll(".step");
     for(let i = 0; i < steps.length; i++) {
-        if(i == nextIndex) {
-            steps[i].classList.add('show');
+        if(i == step) {
+            steps[i].style.display = "block";
         } else {
-            steps[i].classList.remove('show');
+            steps[i].style.display = "none";
         }
     }
 }
 
-// 各ステップへのボタンにイベントリスナーを設定
-toStep2.addEventListener('click', function() { console.log("toStep2 button clicked"); switchStep(1); });
-toStep3.addEventListener('click', function() { console.log("toStep3 button clicked"); switchStep(2); });
-toStep4.addEventListener('click', function() { console.log("toStep4 button clicked"); switchStep(3); });
-toStep5.addEventListener('click', function() { console.log("toStep5 button clicked"); switchStep(4); });
-toStep6.addEventListener('click', function() { 
-    console.log("toStep6 button clicked");
-    let fee = calculateFee();
-    result.textContent = fee + "円";
-    calculationProcess.textContent = `(${ticketPrice.value} * ${numberOfSeats.value} * ${numberOfShows.value} + ${subsidy.value}) * ${widthSetting.value}`;
-    switchStep(5); 
-    console.log('Final calculation:', fee);
+// ステップ間の移動イベントリスナー
+document.getElementById("toStep2").addEventListener("click", function() {
+    switchStep(1);
+});
+// 各ステップ間の移動イベントリスナーを設定します...
+document.getElementById("showResult").addEventListener("click", function() {
+    document.getElementById("fee").innerText = calculateFee();
+    switchStep(8);
 });
 
-// 計算処理
+// リセットイベントリスナー
+document.getElementById("reset").addEventListener("click", function() {
+    switchStep(0);
+});
+
+// 著作権料計算関数
 function calculateFee() {
-    console.log('Calculating fee...');
-    // 高校演劇を選択した場合は5000円
+    // ハイスクール演劇を選択した場合は5000円
     if(typeSetting.value == "highSchool") {
         console.log('Type is high school, returning 5000');
         return 5000;
     }
 
     // 計算結果（5000円未満は切り上げ）
-    let fee = Math.ceil((Number(ticketPrice.value) * Number(numberOfSeats.value) * Number(numberOfShows.value) + Number(subsidy.value)) * Number(widthSetting.value) / 5000) * 5000;
+    let fee = Math.ceil((Number(ticketPrice.value) * Number(numberOfSeats.value) * Number(numberOfShows.value) + Number(subsidy.value) + Number(commission.value)) * Number(widthSetting.value) / 5000) * 5000;
     console.log('Calculated fee:', fee);
 
     return fee;
