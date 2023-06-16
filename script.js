6e0be8b833b4226309b4fac9b319aa0b85107f15
Taisuke Ozaki <97082787+ozata92@users.noreply.github.com>
2023-06-17 01:37:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,60 @@
 console.log("Script.js loaded");
 
-// ステップの変更関数
-function switchStep(step) {
-    let steps = document.querySelectorAll(".step");
-    for(let i = 0; i < steps.length; i++) {
-        if(i == step) {
-            steps[i].style.display = "block";
-        } else {
-            steps[i].style.display = "none";
-        }
-    }
+// 全てのステップ要素を取得
+const steps = document.querySelectorAll(".step");
+
+// 次のステップに進む関数
+function nextStep(currentStep) {
+	console.log("現在のステップ:", currentStep);
+
+	// 現在のステップを非アクティブにする
+	steps[currentStep - 1].classList.remove("active");
+
+	// 次のステップをアクティブにする
+	steps[currentStep].classList.add("active");
 }
 
-// ステップ間の移動イベントリスナー
-document.getElementById("toStep2").addEventListener("click", function() {
-    switchStep(1);
-});
-// 各ステップ間の移動イベントリスナーを設定
-document.getElementById("showResult").addEventListener("click", function() {
-    document.getElementById("fee").innerText = calculateFee();
-    switchStep(8);
-});
+// 計算と結果表示の関数
+function calculate() {
+	console.log("計算開始");
 
-// リセットイベントリスナー
-document.getElementById("reset").addEventListener("click", function() {
-    switchStep(0);
-});
+	// 変数を取得
+	const ticketPrice = parseFloat(document.getElementById("ticketPrice").value);
+	const seatCount = parseInt(document.getElementById("seatCount").value);
+	const performanceCount = parseInt(document.getElementById("performanceCount").value);
+	const grant = parseFloat(document.getElementById("grant").value);
+	const sponsor = parseFloat(document.getElementById("sponsor").value);
+	const range = parseFloat(document.getElementById("range").value);
+	const category = document.getElementById("category").value;
 
-// 上演料計算関数
-function calculateFee() {
-    // 高校演劇を選択した場合は5000円
-    if(typeSetting.value == "highSchool") {
-        console.log('Type is high school, returning 5000');
-        return 5000;
-    }
+	console.log("入力値:", ticketPrice, seatCount, performanceCount, grant, sponsor, range, category);
 
-    // 計算結果（5000円未満は切り上げ）
-    let fee = Math.ceil((Number(ticketPrice.value) * Number(numberOfSeats.value) * Number(numberOfShows.value) + Number(subsidy.value) + Number(commission.value)) * Number(widthSetting.value) / 5000) * 5000;
-    console.log('Calculated fee:', fee);
+	// 計算
+	let total = 0;
+	if (category === "highschool") {
+		total = 5000;
+	} else {
+		total = (ticketPrice * seatCount * performanceCount + grant + sponsor) * (category === "other" ? range * 0.6 : range);
+	}
 
-    return fee;
+	// 5000円未満の端数を切り上げ
+	total = Math.ceil(total / 5000) * 5000;
+
+	console.log("計算結果:", total);
+
+	// 計算過程の表示
+	document.getElementById("ticketPriceProcess").textContent = ticketPrice;
+	document.getElementById("seatCountProcess").textContent = seatCount;
+	document.getElementById("performanceCountProcess").textContent = performanceCount;
+	document.getElementById("grantProcess").textContent = grant;
+	document.getElementById("sponsorProcess").textContent = sponsor;
+	document.getElementById("rangeProcess").textContent = range;
+
+	// 結果を表示
+	document.getElementById("total").querySelector("span").textContent = total;
+
+	// 計算結果ステップを表示
+	steps[6].classList.remove("active");
+	document.getElementById("result").classList.add("active");
+	document.getElementById("calculationProcess").classList.add("active");
 }
-
-// ページ読み込み時に初期ステップを表示
-switchStep(0);
-console.log('Initial step shown');
